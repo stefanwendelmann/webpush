0b804270f812434e082ecbdaa433de131aad0c47
Stefan Höltker <SHoeltker@itout.de>
2020-12-16 08:49:51 +0100

    main: Anpassung Look & Feel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hallo Hendrik !"/>
+    <HelloWorld msg="Hallo Thorsten !"/>
   </div>
 </template>
 
